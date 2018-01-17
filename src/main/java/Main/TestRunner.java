f20683b59ede2b9322b5260dfc294cec0f1b3591
nibandhan <35026252+nibandhan@users.noreply.github.com>
2018-01-17 14:38:33 +0530

    Update TestRunner.java

--- src/main/java/Main/TestRunner.java
+++ src/main/java/Main/TestRunner.java
@@ -7,7 +7,7 @@ import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
 
-@CucumberOptions(features="Feature/Cucumber.feature", glue= "StepDeff")
+@CucumberOptions(features="Feature/Cucumber.feature", glue= "StepDef")
 
 public class TestRunner {
 
